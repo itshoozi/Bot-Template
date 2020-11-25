b6de1daf77a33b88197c33c23a4fbd9a305a63f3
pringle143 <39378302+pringle143@users.noreply.github.com>
2020-11-25 15:00:07 -0600

    added stat counter

---
+++ assets/js/Counting.js
@@ -0,0 +1,19 @@
+jQuery(window).scroll(startCounter);
+function startCounter() {
+    var hT = jQuery('.love_counter').offset().top,
+        hH = jQuery('.love_counter').outerHeight(),
+        wH = jQuery(window).height();
+    if (jQuery(window).scrollTop() > hT+hH-wH) {
+        jQuery(window).off("scroll", startCounter);
+        jQuery('.love_count').each(function () {
+            var $this = jQuery(this);
+            jQuery({ Counter: 0 }).animate({ Counter: $this.text() }, {
+                duration: 2000,
+                easing: 'swing',
+                step: function () {
+                    $this.text(Math.ceil(this.Counter) + '+');
+                }
+            });
+        });
+    }
+}
